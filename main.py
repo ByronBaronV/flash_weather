7807425e93e83c4be412d4a78f9c3f13659b69d6
Byron <byron.baron.seo@gmail.com>
2024-11-12 09:36:37 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Sample weather data - in a real app, this would come from an API
+weather_data = {
+    'temperature': 22,
+    'condition': 'Sunny',
+    'humidity': 45,
+    'wind_speed': 15
+}
+
+@app.route('/')
+def index():
+    return render_template('weather.html',
+                           weather=weather_data)
+
+# DEMO AREA
+if __name__ == '__main__':
+    app.run(debug=True)
+
